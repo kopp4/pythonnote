34cedd4810914f105ce20d5b11aed5970939ebe7
taltalasuka <taltalasuka@qq.com>
2021-10-17 21:29:21 +0800

    Add JavaNote
    Create some java stuff
    'n that's all

---
+++ javanote/src/javaadvanced/HashMap.java
@@ -0,0 +1,12 @@
+package javaadvanced;
+
+
+
+/**
+ * @Author: taltalasuka
+ * @Date: 2021/10/17 21:26
+ */
+public class HashMap {
+    private String URL = "https://www.runoob.com/java/java-hashmap.html";
+    
+}
