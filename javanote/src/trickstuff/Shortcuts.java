853a136b1faf6f42a3376ee51b45859e7c490fcb
taltalasuka <taltalasuka@qq.com>
2021-10-17 21:54:20 +0800

    Java Shortcuts

---
+++ javanote/src/trickstuff/Shortcuts.java
@@ -0,0 +1,28 @@
+package trickstuff;
+
+/**
+ * @Author: taltalasuka
+ * @Date: 2021/10/17 21:41
+ */
+public class Shortcuts {
+
+}
+/**
+ * Rename : Alt + F6
+ * **Correcting** : Alt + Enter
+ * Extend selection : after selected : Ctrl + W
+ * **Locating error** : F2
+ * Find Usage : Alt + F7
+ * Go to line: Ctrl + G
+ * Find in path : Ctrl + Shift + F
+ * Search : Shift + Shift
+ * Recent file : Ctrl + E
+ * **Surround with** : Ctrl + Alt + T
+ * Show in explorer : Ctrl + Alt + F12
+ * **Show file structure** : Ctrl + F12
+ * **Replace** : Ctrl + R
+ * **Bookmark** : Ctrl + F11
+ * **Comment** : Ctrl + J
+ *
+ * Tooooo much
+ */
